b585cab84cec7e784b56a4741bba0fa549ddc362
Asaad Budayr <asad_289@hotmail.com>
2019-01-13 03:49:53 -0800

    Feat: Create initial Dashboard component
    The component only displays questionIds

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import '../App.css';
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
+import Dashboard from './Dashboard'
 
 class App extends Component {
   componentDidMount() {
@@ -10,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      Start Code
+        <Dashboard />
       </div>
     );
   }
