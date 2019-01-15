95b96294c90e953922b3659650ac9dea033677e5
Asaad Budayr <asad_289@hotmail.com>
2019-01-15 08:10:32 -0800

    Feat: Create actions to handle answering questions

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,34 +1,56 @@
-import { _saveQuestion } from '../utils/_DATA'
-import { showLoading, hideLoading } from 'react-redux-loading-bar'
-export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
-export const ADD_QUESTION = 'ADD_QUESTION'
+import { _saveQuestion, _saveQuestionAnswer } from "../utils/_DATA";
+import { showLoading, hideLoading } from "react-redux-loading-bar";
+export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS";
+export const ADD_QUESTION = "ADD_QUESTION";
+export const ANSWER_QUESTION = "ANSWER_QUESTION";
 
-export function receiveQuestions (questions) {
+export function receiveQuestions(questions) {
   return {
     type: RECEIVE_QUESTIONS,
-    questions,
-  }
+    questions
+  };
 }
 
-export function addQuestion (question) {
+export function addQuestion(question) {
   return {
     type: ADD_QUESTION,
-    question,
-  }
+    question
+  };
+}
+export function answerQuestion({ qid, answer, authedUser }) {
+  return {
+    type: ADD_QUESTION,
+    qid,
+    answer,
+    authedUser
+  };
 }
 
-export function handleAddQuestion (question) {
-  console.log('This os the question: ', question)
+export function handleAddQuestion(question) {
   return (dispatch, getState) => {
-    const { authedUser } = getState()
-    const {optionOneText, optionTwoText} = question
-    dispatch(showLoading())
+    const { authedUser } = getState();
+    const { optionOneText, optionTwoText } = question;
+    dispatch(showLoading());
     return _saveQuestion({
       optionOneText,
       optionTwoText,
-      author: authedUser,
+      author: authedUser
     })
-    .then((question) => dispatch(addQuestion(question)))
-    .then(() => dispatch(hideLoading()))
-  }
+      .then(question => dispatch(addQuestion(question)))
+      .then(() => dispatch(hideLoading()));
+  };
+}
+
+export function handleAnswerQuestion(qid, answer) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState();
+    dispatch(showLoading());
+    return _saveQuestionAnswer({
+      authedUser,
+      qid,
+      answer
+    })
+      .then(() => dispatch(answerQuestion({ authedUser, qid, answer })))
+      .then(() => dispatch(hideLoading()));
+  };
 }
