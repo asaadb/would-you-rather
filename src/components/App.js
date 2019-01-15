70d94b866c1045e52af7f3d48705ab5484db0697
Asaad Budayr <asad_289@hotmail.com>
2019-01-15 10:19:39 -0800

    Feat: Use react route to connect pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,40 @@
-import React, { Component } from 'react';
-import '../App.css';
-import { connect } from 'react-redux'
-import { handleInitialData } from '../actions/shared'
-import Dashboard from './Dashboard'
+import React, { Component, Fragment } from "react";
+import "../App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import LoadingBar from "react-redux-loading";
+import { handleInitialData } from "../actions/shared";
+import Dashboard from "./Dashboard";
+import NewPoll from "./NewPoll";
+import Nav from "./Nav";
 
 class App extends Component {
   componentDidMount() {
-    this.props.dispatch(handleInitialData())
+    this.props.dispatch(handleInitialData());
   }
   render() {
     return (
-      <div className="App">
-      {this.props.loading === true
-        ? <div>Loading...</div>
-        : <Dashboard />}
-      </div>
+      <Router>
+        <Fragment>
+          <LoadingBar />
+          <div className="App">
+            <Nav />
+            {this.props.loading === true ? null : (
+              <div>
+                <Route path="/" exact component={Dashboard} />
+                <Route path="/new" exact component={NewPoll} />
+              </div>
+            )}
+          </div>
+        </Fragment>
+      </Router>
     );
   }
 }
 
-function mapStateToProps ({ authedUser }) {
+function mapStateToProps({ authedUser }) {
   return {
     loading: authedUser === null
-  }
+  };
 }
 export default connect(mapStateToProps)(App);
