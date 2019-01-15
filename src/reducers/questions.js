a97540286d1da2b1af536da9620cf8a445f6138d
Asaad Budayr <asad_289@hotmail.com>
2019-01-15 08:13:53 -0800

    Feat: Make the reducer handle answering questions

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,18 +1,34 @@
-import { RECEIVE_QUESTIONS, ADD_QUESTION } from '../actions/questions'
+import {
+  RECEIVE_QUESTIONS,
+  ADD_QUESTION,
+  ANSWER_QUESTION
+} from "../actions/questions";
 
-export default function questions (state = {}, action) {
+export default function questions(state = {}, action) {
   switch (action.type) {
-    case RECEIVE_QUESTIONS :
+    case RECEIVE_QUESTIONS:
       return {
         ...state,
         ...action.questions
-      }
-    case ADD_QUESTION :
+      };
+    case ADD_QUESTION:
       return {
         ...state,
-        [action.question.id]: action.question,
-      }
-      default :
-      return state
+        [action.question.id]: action.question
+      };
+    case ANSWER_QUESTION:
+      const { qid, answer, authedUser } = action;
+      return {
+        ...state,
+        [qid]: {
+          ...state[qid],
+          [answer]: {
+            ...state[qid][answer],
+            votes: state[qid][answer].votes.concat([authedUser])
+          }
+        }
+      };
+    default:
+      return state;
   }
 }
