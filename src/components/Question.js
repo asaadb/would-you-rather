08b81fcc15f82db31c7bed2b034ac364b85f182b
Asaad Budayr <asad_289@hotmail.com>
2019-01-16 04:07:42 -0800

    Feat: Add a link to the question/:id

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,51 +1,45 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { formatDate } from '../utils/helpers'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { formatDate } from "../utils/helpers";
+import { Link } from "react-router-dom";
+
 class Question extends Component {
   render() {
-
-    const { question, user } = this.props
-     // console.log('Question: ', question);
-     // console.log('User: ', user);
-    const { timestamp, optionOne, optionTwo, id } = question
-    const { name, avatarURL } = user
-    console.log('author: ',name)
-    console.log('timestamp: ',timestamp)
-    console.log('optionOne: ',optionOne)
-    console.log('optionTwo: ', optionTwo)
-    console.log('avatar: ', avatarURL)
-    console.log('id: ', id)
+    const { question, user } = this.props;
+    const { timestamp, optionOne, optionTwo, id } = question;
+    const { name, avatarURL } = user;
     return (
-      <div className='poll'>
-        <div className='poll-container'>
-          <img
-            src={avatarURL}
-            alt={`avatar of ${name}`}
-            className='avatar'
-          />
-          <div className='poll-info'>
+      <div className="poll">
+        <div className="poll-container">
+          <img src={avatarURL} alt={`avatar of ${name}`} className="avatar" />
+          <div className="poll-info">
             <span>{name}</span>
             <div>{formatDate(timestamp)}</div>
           </div>
         </div>
-        <p className='questions'> "{optionOne.text}"
-          <br/>
+        <p className="questions">
+          {" "}
+          "{optionOne.text}"
+          <br />
           or
-          <br/>
-           "{optionTwo.text}"
+          <br />"{optionTwo.text}"
         </p>
+        <Link to={`/question/${id}`} className="question-link">
+          {" "}
+          To Link
+        </Link>
       </div>
-    )
+    );
   }
 }
 
-function mapStateToProps ({ authedUser, users,  questions }, {id}) {
-  const question = questions[id]
+function mapStateToProps({ authedUser, users, questions }, { id }) {
+  const question = questions[id];
   return {
     authedUser,
     question: question,
     user: users[question.author]
-  }
+  };
 }
 
-export default connect(mapStateToProps)(Question)
+export default connect(mapStateToProps)(Question);
