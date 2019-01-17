96ed1628e6461ffc7223832ff3ba3aac8193c116
Asaad Budayr <asad_289@hotmail.com>
2019-01-17 12:59:18 -0800

    Feat: Render all needed info for AnsweredQuestion

--- src/components/AnsweredQuestion.js
+++ src/components/AnsweredQuestion.js
@@ -1,21 +1,53 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { formatDate } from "../utils/helpers";
+import { CircleMeter } from 'react-svg-meters';
+import { FaRegCheckCircle } from "react-icons/fa";
 
 class AnsweredQuestion extends Component {
   render() {
+    const { question, user, authedUser } = this.props;
+    const { timestamp, optionOne, optionTwo } = question;
+    const { name, avatarURL } = user;
+    const totalVotes = optionOne.votes.length + optionTwo.votes.length;
     return (
       <div className="Answered">
-        {`AnsweredQuestionId: ${this.props.question.id}`}
+        <div className="poll-container">
+          <img src={avatarURL} alt={`avatar of ${name}`} className="avatar" />
+          <div className="poll-info">
+            <span>{name}</span>
+            <div>{formatDate(timestamp)}</div>
+          </div>
+        </div>
+        <ul>
+          <li>
+            <CircleMeter value={Math.round((optionOne.votes.length / totalVotes) * 100)} size={100} />
+            {optionOne.votes.includes(authedUser) ? (
+              <span style={{ color: "red" }}> <FaRegCheckCircle/> You</span>
+            ) : null}
+            <p>{optionOne.text}</p>
+            <span>{`Votes ${optionOne.votes.length}`}</span>
+          </li>
+          <li>
+            <CircleMeter value={Math.round((optionTwo.votes.length / totalVotes) * 100)} size={100} />
+            {optionTwo.votes.includes(authedUser) ? (
+              <span style={{ color: "red" }}> <FaRegCheckCircle/> You</span>
+            ) : null}
+            <p>{optionTwo.text}</p>
+            <span>{`Votes ${optionTwo.votes.length}`}</span>
+          </li>
+        </ul>
       </div>
     );
   }
 }
 
-function mapStateToProps({ questions, users }, { id }) {
+function mapStateToProps({ authedUser, users, questions }, { id }) {
   const question = questions[id];
   return {
-    question,
-    author: question
+    authedUser,
+    question: question,
+    user: users[question.author]
   };
 }
 
