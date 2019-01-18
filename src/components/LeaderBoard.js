8a4117435a7e1ec815a6f5a45fae319b3d3f1a7a
Asaad Budayr <asad_289@hotmail.com>
2019-01-18 04:52:20 -0800

    Feat: Create LeaderBoard component
    This component sorts users ids by score, then passes them to UserInfo
    component

---
+++ src/components/LeaderBoard.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import UserInfo from "./UserInfo";
+
+class LeaderBoard extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Leader Board</h2>
+        <div>
+          {this.props.sortedIds.map(id => (
+            <UserInfo key={id} id={id} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ users }) {
+  const sortedIds = Object.keys(users).sort((a, b) => {
+    const answersA = users[a].answers;
+    const answersB = users[b].answers;
+    const scoreA = Object.keys(answersA).length + users[a].questions.length;
+    const scoreB = Object.keys(answersB).length + users[b].questions.length;
+    return scoreB - scoreA;
+  });
+  return {
+    sortedIds
+  };
+}
+
+export default connect(mapStateToProps)(LeaderBoard);
