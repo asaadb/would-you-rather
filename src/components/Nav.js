4bffd1b3aeb6a54407feba379a0da18d4757fa08
Asaad Budayr <asad_289@hotmail.com>
2019-01-20 11:29:34 -0800

    Feat: Improve the styling of the Nav Bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,133 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import { IoMdMenu } from "react-icons/io";
+import { NavLink } from "react-router-dom";
+import { MdLibraryAdd, MdHome, MdDashboard } from "react-icons/md";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import Avatar from "@material-ui/core/Avatar";
+import { connect } from "react-redux";
+import { withStyles } from "@material-ui/core/styles";
 
-export default function Nav () {
-  return (
-    <nav className='nav'>
-      <ul>
-        <li>
-          <NavLink to='/' exact activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/new' activeClassName='active'>
-            New Poll
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/leaderboard' activeClassName='active'>
-            Leader Board
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  )
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1,
+    margin: 10,
+    marginLeft: 20
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginLight: 20
+  },
+  list: {
+    width: 250
+  },
+  avatar: {
+    margin: 10
+  }
+};
+class Nav extends React.Component {
+  state = {
+    left: false
+  };
+
+  toggleDrawer = (side, open) => () => {
+    this.setState({
+      [side]: open
+    });
+  };
+
+  render() {
+    const { classes, user } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <IconButton
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="Menu"
+            >
+              <div>
+                <IoMdMenu onClick={this.toggleDrawer("left", true)} />
+                <Drawer
+                  open={this.state.left}
+                  onClose={this.toggleDrawer("left", false)}
+                >
+                  <div
+                    tabIndex={0}
+                    role="button"
+                    onClick={this.toggleDrawer("left", false)}
+                    onKeyDown={this.toggleDrawer("left", false)}
+                  >
+                    <div className={classes.list}>
+                      <List>
+                        <ListItem>
+                          <NavLink to="/" exact activeClassName="active">
+                            <ListItemIcon>
+                              <MdHome />
+                            </ListItemIcon>
+                            Home
+                          </NavLink>
+                        </ListItem>
+                        <ListItem>
+                          <NavLink to="/new" activeClassName="active">
+                            <ListItemIcon>
+                              <MdLibraryAdd />
+                            </ListItemIcon>
+                            New Poll
+                          </NavLink>
+                        </ListItem>
+                        <ListItem>
+                          <NavLink to="/leaderboard" activeClassName="active">
+                            <ListItemIcon>
+                              <MdDashboard />
+                            </ListItemIcon>
+                            Leader Board
+                          </NavLink>
+                        </ListItem>
+                      </List>
+                    </div>
+                  </div>
+                </Drawer>
+              </div>
+            </IconButton>
+            <Typography
+              variant="h6"
+              color="inherit"
+              className={classes.grow}
+              align="left"
+            >
+              Would You Rather?
+            </Typography>
+            <span className="user-name-nav">{`Hello, ${user.name}`}</span>
+            <Avatar
+              alt={`${user.name} avatar`}
+              src={user.avatarURL}
+              className="avatar"
+            />
+            <Button color="inherit"> Logout</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 }
+
+function mapStateToProps({ authedUser, users }) {
+  return {
+    user: users[authedUser]
+  };
+}
+
+export default connect(mapStateToProps)(withStyles(styles)(Nav));
