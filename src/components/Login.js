75d131a2c6669c27f162e5be3b874c5f86dfe4ec
Asaad Budayr <asad_289@hotmail.com>
2019-01-23 04:09:12 -0800

    Feat: Improve the styling of the Login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,40 +1,87 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { setAuthedUser } from '../actions/authedUser'
+import { setAuthedUser } from "../actions/authedUser";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { MdPerson } from "react-icons/md";
+
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  appBar: {
+    backgroundColor: "#605052"
+  },
+  grow: {
+    flexGrow: 1,
+    fontSize: "1.5rem"
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
+};
+
 class Login extends Component {
   state = {
-    authedUser: ''
+    authedUser: "",
+    imgSrc: "/favicon.ico"
   };
   handleSubmit = event => {
     event.preventDefault();
     const { authedUser } = this.state;
-    const { dispatch } = this.props
-    dispatch(setAuthedUser(authedUser))
+    const { dispatch } = this.props;
+    dispatch(setAuthedUser(authedUser));
   };
-  handleSelect = (e) => {
-    const val = e.target.value
+  handleSelect = e => {
+    const val = e.target.value;
+    const img = this.props.users[val].avatarURL;
     this.setState(() => ({
-      authedUser: val
-    }))
-  }
+      authedUser: val,
+      imgSrc: img
+    }));
+  };
+  const;
   render() {
-    const { users, ids } = this.props;
+    const { users, ids, classes } = this.props;
     return (
       <div>
-        <h2>Login</h2>
-        <span>Select a user</span>
-        <form onSubmit={this.handleSubmit}>
-          <select placeholder='Select' onChange={this.handleSelect}>
-            {ids.map(id => (
-              <option value={users[id].id} key={users[id].id}>
-                {users[id].name}
+        <div className={classes.root}>
+          <AppBar className={classes.appBar} position="static">
+            <Toolbar>
+              <Typography variant="h6" color="inherit" className={classes.grow}>
+                Would You Rather ?
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </div>
+        <main className="login card">
+          <div className="card-header">
+            <MdPerson className="header-icon" />
+            <h3>Sign In</h3>
+          </div>
+          <form onSubmit={this.handleSubmit} className="login-form">
+            <img src={this.state.imgSrc} alt="avatar" className="avatar" />
+            <select onChange={this.handleSelect}>
+              <option hidden value="default">
+                Select a user...
               </option>
-            ))}
-          </select>
-          <button>
-          Go
-          </button>
-        </form>
+              {ids.map(id => (
+                <option value={users[id].id} key={users[id].id}>
+                  {users[id].name}
+                </option>
+              ))}
+            </select>
+            <button
+              disabled={this.state.authedUser === ""}
+              className="question-btn sign-in"
+            >
+              <span>Sign In</span>
+            </button>
+          </form>
+        </main>
       </div>
     );
   }
@@ -43,8 +90,8 @@ class Login extends Component {
 function mapStateToProps({ users }) {
   return {
     users,
-    ids:Object.keys(users)
+    ids: Object.keys(users)
   };
 }
 
-export default connect(mapStateToProps)(Login);
+export default connect(mapStateToProps)(withStyles(styles)(Login));
