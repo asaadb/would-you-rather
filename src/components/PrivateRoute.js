8cc894e6aa41c82114fa80aea28508d0f0e0b28d
Asaad Budayr <asad_289@hotmail.com>
2019-01-25 13:55:14 -0800

    Feat: Create PrivateRoute  component
    This component protect the route and redirect to the login page is the
    user is not authenticated

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { isAuth } from "../utils/api";
+import { Route, Redirect } from "react-router-dom";
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      isAuth.isAuthenticated === true ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/",
+            state: { from: props.location }
+          }}
+        />
+      )
+    }
+  />
+);
+
+export default PrivateRoute;
